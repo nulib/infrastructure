915e601cb953410cd2d512e8a52576c77c9aaff1
Michael B. Klein <mbklein@gmail.com>
2021-08-30 13:50:59 -0500

    Update dbschema module to use data_services component

--- modules/dbschema/main.tf
+++ modules/dbschema/main.tf
@@ -8,19 +8,19 @@ data "terraform_remote_state" "core" {
   }
 }
 
-data "terraform_remote_state" "db" {
+data "terraform_remote_state" "data_services" {
   backend = "s3"
 
   config = {
     bucket = var.state_bucket
-    key    = "env:/${terraform.workspace}/db.tfstate"
+    key    = "env:/${terraform.workspace}/data_services.tfstate"
     region = var.aws_region
   }
 }
 
 locals {
-  core = data.terraform_remote_state.core.outputs
-  db   = data.terraform_remote_state.db.outputs
+  core            = data.terraform_remote_state.core.outputs
+  data_services   = data.terraform_remote_state.data_services.outputs
 }
 
 resource "random_string" "role_password" {
@@ -44,22 +44,22 @@ BEGIN
     SELECT FROM pg_catalog.pg_auth_members a
       JOIN pg_catalog.pg_roles b ON a.roleid = b.oid
       JOIN pg_catalog.pg_roles c ON a.member = c.oid
-      WHERE c.rolname = '${local.db.admin_user}' AND b.rolname = '${var.schema}'
+      WHERE c.rolname = '${local.data_services.postgres.admin_user}' AND b.rolname = '${var.schema}'
   ) THEN
-    GRANT ${var.schema} TO ${local.db.admin_user};
+    GRANT ${var.schema} TO ${local.data_services.postgres.admin_user};
   END IF;
 END
 \$do\$;
 CREATE DATABASE ${var.schema} OWNER ${var.schema};
 EOF
 
-  psql            = "PGPASSWORD='${local.db.admin_password}' psql -U ${local.db.admin_user} -h ${local.db.address} -p ${local.db.port} postgres"
+  psql            = "PGPASSWORD='${local.data_services.postgres.admin_password}' psql -U ${local.data_services.postgres.admin_user} -h ${local.data_services.postgres.address} -p ${local.data_services.postgres.port} postgres"
   create_command  = "echo \"${local.create_script}\" | ${local.psql}"
 }
 
 resource "null_resource" "this_database" {
   triggers = {
-    value = local.db.address
+    value = local.data_services.postgres.address
   }
 
   connection {
