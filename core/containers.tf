4e3a0ce289b7bacd1132399d91da660068e947ba
Michael B. Klein <mbklein@gmail.com>
2021-09-16 19:10:12 -0500

    Add lifecycle rules to ECR repositories

--- core/containers.tf
+++ core/containers.tf
@@ -1,8 +1,35 @@
+locals {
+  repositories = toset(["arch", "avr", "fcrepo4", "meadow", "solr", "zookeeper"])
+}
+
 resource "aws_ecr_repository" "nulib_images" {
-  for_each                = toset(["arch", "avr", "fcrepo4", "meadow", "solr", "zookeeper"])
+  for_each                = local.repositories
   name                    = each.key
   image_tag_mutability    = "MUTABLE"
   tags                    = merge(local.tags)
+}
+
+resource "aws_ecr_lifecycle_policy" "nulib_image_expiration" {
+  for_each    = local.repositories
+  repository  = aws_ecr_repository.nulib_images[each.key].name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images after 7 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 7
+        }
+        action = {
+          type        = "expire"
+        }
+      }
+    ]
+  })
 }
 
 data "aws_iam_policy_document" "ecs_assume_role" {
