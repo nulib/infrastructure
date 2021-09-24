fab53b943c018f2427135986e92e0548f1ae8e51
Michael B. Klein <mbklein@gmail.com>
2021-09-24 12:47:16 -0500

    Fix CloudWatch alarms

--- monitoring/main.tf
+++ monitoring/main.tf
@@ -27,6 +27,12 @@ data "aws_lb" "load_balancer" {
   name        = each.key
 }
 
+locals {
+  keys = flatten([ for cluster, services in local.secrets.services : [ for service in services : "${cluster}.${service}" ] ])
+  values = flatten([ for cluster, services in local.secrets.services : [ for service in services : { cluster: cluster, service: service } ] ])
+  services = zipmap(local.keys, local.values)
+}
+
 resource "aws_cloudwatch_metric_alarm" "load_balancer_5xx" {
   for_each              = toset(local.secrets.load_balancers)
   alarm_name            = "${each.key}-LoadBalancer5XX"
@@ -37,6 +43,7 @@ resource "aws_cloudwatch_metric_alarm" "load_balancer_5xx" {
   period                = 60
   statistic             = "Sum"
   threshold             = 180
+  treat_missing_data    = "notBreaching"
 
   dimensions = {
     LoadBalancer = data.aws_lb.load_balancer[each.key].arn_suffix
@@ -46,11 +53,11 @@ resource "aws_cloudwatch_metric_alarm" "load_balancer_5xx" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
-  for_each              = toset(local.secrets.services)
+  for_each              = local.services
 
   actions_enabled       = local.secrets.actions_enabled
   alarm_actions         = local.secrets.alarm_actions
-  alarm_name            = "${each.key}-CPUUtilization"
+  alarm_name            = "${each.value.service}-CPUUtilization"
   comparison_operator   = "GreaterThanOrEqualToThreshold"
   evaluation_periods    = 3
   metric_name           = "CPUUtilization"
@@ -60,18 +67,19 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
   threshold             = 90
 
   dimensions = {
-    ServiceName = each.key
+    ClusterName = each.value.cluster
+    ServiceName = each.value.service
   }
 
   tags = local.tags
 }
 
 resource "aws_cloudwatch_metric_alarm" "memory_utilization" {
-  for_each              = toset(local.secrets.services)
+  for_each              = local.services
 
   actions_enabled       = local.secrets.actions_enabled
   alarm_actions         = local.secrets.alarm_actions
-  alarm_name            = "${each.key}-MemoryUtilization"
+  alarm_name            = "${each.value.service}-MemoryUtilization"
   comparison_operator   = "GreaterThanOrEqualToThreshold"
   evaluation_periods    = 3
   metric_name           = "MemoryUtilization"
@@ -81,7 +89,8 @@ resource "aws_cloudwatch_metric_alarm" "memory_utilization" {
   threshold             = 90
 
   dimensions = {
-    ServiceName = each.key
+    ClusterName = each.value.cluster
+    ServiceName = each.value.service
   }
 
   tags = local.tags
