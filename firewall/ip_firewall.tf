19eb85d054f460ac230b6535cd7f5b0e0a3f2ac3
Michael B. Klein <mbklein@gmail.com>
2022-01-19 12:43:08 -0600

    Add IPv6 addresses to staging firewall

--- firewall/ip_firewall.tf
+++ firewall/ip_firewall.tf
@@ -1,10 +1,20 @@
 resource "aws_wafv2_ip_set" "nul_ip_set" {
   count              = local.secrets.firewall_type == "IP" ? 1 : 0
   name               = "nul-ips"
-  description        = "NU Library IP Addresses"
+  description        = "NU Library IPv4 Addresses"
   scope              = "REGIONAL"
   ip_address_version = "IPV4"
   addresses          = local.secrets.nul_ips
+  tags               = local.tags
+}
+
+resource "aws_wafv2_ip_set" "nul_ipv6_set" {
+  count              = local.secrets.firewall_type == "IP" ? 1 : 0
+  name               = "nul-ips-v6"
+  description        = "NU Library IPv6 Addresses"
+  scope              = "REGIONAL"
+  ip_address_version = "IPV6"
+  addresses          = local.secrets.nul_ips_v6
   tags               = local.tags
 }
 
@@ -51,8 +61,28 @@ resource "aws_wafv2_web_acl" "ip_firewall" {
   }
 
   rule {
-    name     = "allow-rdc-home-ips"
+    name     = "allow-nul-ips-v6"
     priority = 2
+
+    action {
+      allow {}
+    }
+
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.nul_ipv6_set[0].arn
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "Allow_NUL_IPv6s"
+      sampled_requests_enabled   = true
+    }
+  }
+  rule {
+    name     = "allow-rdc-home-ips"
+    priority = 3
 
     action {
       allow {}
