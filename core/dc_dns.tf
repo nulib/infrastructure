3916fc539f05e7b6702350e6071585d7452f057e
Michael B. Klein <mbklein@gmail.com>
2023-01-11 21:18:35 +0000

    Fix the way dc wildcard cert validation is done

--- core/dc_dns.tf
+++ core/dc_dns.tf
@@ -8,19 +8,17 @@ resource "aws_acm_certificate" "dc_wildcard" {
   }
 }
 
+locals {
+  validation_record = length(aws_route53_zone.dc_zone) == 0 ? {} : tolist(aws_acm_certificate.dc_wildcard[0].domain_validation_options)[0]
+}
+
 resource "aws_route53_record" "dc_wildcard_cert_validation" {
-  for_each = length(aws_route53_zone.dc_zone) == 0 ? {} : {
-    for dvo in aws_acm_certificate.dc_wildcard[0].domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+  count = length(aws_route53_zone.dc_zone)
 
   zone_id = aws_route53_zone.dc_zone[0].zone_id
-  type    = each.value.type
-  name    = each.value.name
-  records = [each.value.record]
+  type    = local.validation_record.resource_record_type
+  name    = local.validation_record.resource_record_name
+  records = [local.validation_record.resource_record_value]
   ttl     = 300
 }
 
@@ -28,12 +26,4 @@ resource "aws_route53_zone" "dc_zone" {
   count   = var.digital_collections_zone_name == "" ? 0 : 1
   name    = var.digital_collections_zone_name
   tags    = local.tags
-}
-
-resource "aws_route53_record" "dc_cname" {
-  zone_id = aws_route53_zone.dc_zone[0].zone_id
-  name    = "."
-  type    = "CNAME"
-  records = ["dc.stack.rdc.library.northwestern.edu"]
-  ttl     = 60
 }
