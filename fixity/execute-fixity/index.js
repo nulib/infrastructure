246311b5d73bf0f9e025850f6861a20c87bffbb2
Michael B. Klein <mbklein@gmail.com>
2026-08-28 21:32:18 +0000

    Use updated serverless-fixity stack

--- fixity/execute-fixity/index.js
+++ fixity/execute-fixity/index.js
@@ -3,35 +3,33 @@ const stateMachineArn = process.env.stateMachineArn;
 
 const fullyDecode = (str) => decodeURIComponent(str.replace(/\+/g, " "));
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
   var params = {
     stateMachineArn: stateMachineArn,
     input: JSON.stringify({
       Bucket: fullyDecode(event.Records[0].s3.bucket.name),
       Key: fullyDecode(event.Records[0].s3.object.key),
+      Algorithm: ["md5", "sha256"]
     }),
   };
   var stepfunctions = new SFNClient();
   const cmd = new StartExecutionCommand(params);
-  return new Promise((resolve, _reject) => {
-    stepfunctions.send(cmd)
-    .then((data) => {
-      console.log(data);
-      resolve({
-        statusCode: 200,
-        body: JSON.stringify({
-          message: "Step function worked",
-        }),
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      resolve({
-        statusCode: 500,
-        body: JSON.stringify({
-          message: "There was an error",
-        }),
-      });
-    });
-  });
+  try {
+    const data = await stepfunctions.send(cmd);
+    console.log(data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Step function worked",
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "There was an error",
+      }),
+    };
+  }
 };
